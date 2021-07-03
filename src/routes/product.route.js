9fa81026ba8d0c7c1d8aeb3481ba268ae9a816cc
QTienTLS <quangtientls@gmail.com>
2021-07-03 15:42:27 +0700

    cập nhật giỏ hàng ban đầu

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productController = require('../app/controllers/ProductController');
 
 router.get('/all', productController.showAll);
+router.post('/:id/addtocart', productController.addToCart);
 
 module.exports = router;
