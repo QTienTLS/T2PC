6121bf0384572f0daff7662e024ac667b0dca294
QTienTLS <quangtientls@gmail.com>
2021-07-24 15:07:20 +0700

    +1

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -10,7 +10,7 @@ router.put('/event/:id', adminController.updateEvent);
 router.get('/product', adminController.showProduct);
 router.post('/addproduct', adminController.addFullProduct);
 router.post('/addsproduct', adminController.addSemiProduct);
-router.delete('/product/:id', adminController.delPro);
+router.put('/product/del/:id', adminController.delPro);
 router.put('/product/:op/:id', adminController.updatePro);
 router.get('/order/:link', adminController.goToOrder);
 router.put('/banuser/:id', adminController.banUser);
